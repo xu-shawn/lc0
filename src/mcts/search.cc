0ebcd7112ca9a9e9e47d6ec5b359e880aa91c921
Etcaqab <Etcaqab@users.noreply.github.com>
2022-11-21 21:31:35 +0100

    Replace allocated node estimate with the real value

--- src/mcts/search.cc
+++ src/mcts/search.cc
@@ -163,7 +163,6 @@ Search::Search(NodeTree* dag, Network* network,
       searchmoves_(searchmoves),
       start_time_(start_time),
       initial_visits_(root_node_->GetN()),
-      initial_allocated_nodes_(dag->AllocatedNodeCount()),
       root_move_filter_(MakeRootMoveFilter(
           searchmoves_, syzygy_tb_, played_history_,
           params_.GetSyzygyFastPlay(), &tb_hits_, &root_is_in_dtz_)),
@@ -833,7 +832,7 @@ void Search::PopulateCommonIterationStats(IterationStats* stats) {
     }
   }
   stats->total_visits = total_playouts_ + initial_visits_;
-  stats->total_allocated_nodes = total_playouts_ + initial_allocated_nodes_;
+  stats->total_allocated_nodes = dag_->AllocatedNodeCount();
   stats->nodes_since_movestart = total_playouts_;
   stats->batches_since_movestart = total_batches_;
   stats->average_depth = cum_depth_ / (total_playouts_ ? total_playouts_ : 1);
